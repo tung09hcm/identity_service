2cf1bdf79fdc0f05d63c1ec9543ef7005b6bd5ee
tung <tung.bui2004lk@hcmut.edu.vn>
2025-07-17 21:55:27 +0700

    Detailed Validation

--- identity-service/src/main/java/com/example/identity_service/exception/ErrorCode.java
+++ identity-service/src/main/java/com/example/identity_service/exception/ErrorCode.java
@@ -9,12 +9,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
     INVALID_KEY(1000,"Invalid key", HttpStatus.BAD_REQUEST),
     USER_EXISTED(1001,"User existed", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1002,"Username must be at least 3 characters", HttpStatus.BAD_REQUEST),
-    INVALID_PASSWORD(1003, "Message must be at least 8 characters", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1002,"Username must be at least {min} characters", HttpStatus.BAD_REQUEST),
+    INVALID_PASSWORD(1003, "Message must be at least {min} characters", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1004,"User arent existed", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1005, "User is not authenticated", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1006, "You do not have permisson", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1007, "Invalid date of birth", HttpStatus.BAD_REQUEST)
+    INVALID_DOB(1007, "Your age must be at least {min}", HttpStatus.BAD_REQUEST)
     ;
 
     ErrorCode(int code, String message, HttpStatusCode httpStatusCode) {
--- identity-service/src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
+++ identity-service/src/main/java/com/example/identity_service/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.identity_service.exception;
 
 import com.example.identity_service.dto.request.ApiResponse;
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,8 +9,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import org.springframework.security.access.AccessDeniedException;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final static String MIN_ATTRIBUTE = "min";
+
     @ExceptionHandler(value = Exception.class)
     ResponseEntity<ApiResponse<String>> handlingRuntimeException(RuntimeException exception){
         ApiResponse<String> apiResponse = new ApiResponse<>();
@@ -49,15 +56,27 @@ public class GlobalExceptionHandler {
         String message = exception.getFieldError().getDefaultMessage();
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
+        Map<String, Object> attributes = null;
         try{
             errorCode = ErrorCode.valueOf(message);
+            var constraintViolation = exception.getBindingResult()
+                    .getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
         }
         catch(IllegalArgumentException e){
 
         }
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getMessage(), attributes)
+                : errorCode.getMessage());
         apiResponse.setCode(errorCode.getCode());
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message , Map<String, Object> attributes){
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE)) ;
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 
 }
